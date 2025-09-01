a1b6d2dcda18fca35b2e72ed568bb9ab2aa90be6
porterpi <mister.mehedi0@gmail.com>
2025-09-01 06:29:49 +0600

    added variable daclartion and initialization tutorials

---
+++ 2_vars/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// variable declaration and initialization
+	var a int
+	a=10
+
+	// yee he bhi kor sakte hai
+	var b = 20.5
+
+	var c string = "Go is awesome!"
+	var d bool = true
+
+	fmt.Println("Integer:", a)
+	fmt.Println("Float:", b)
+	fmt.Println("String:", c)
+	fmt.Println("Boolean:", d)
+
+	fmt.Println(1+5)
+
+
+	// new feature
+	name := "Porter"
+	fmt.Println("Name:", name)
+}
